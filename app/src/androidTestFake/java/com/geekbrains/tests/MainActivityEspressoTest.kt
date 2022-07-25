3754c615dd33cded65a992b69f4cacd0bd391635
Dmitry Gusev <dmtrxiii@gmail.com>
2022-07-26 00:39:15 +0300

    MainActivity UI tests for fake build type fixed

--- app/src/androidTestFake/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ app/src/androidTestFake/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -4,8 +4,7 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.geekbrains.tests.view.search.MainActivity
 import org.junit.After
@@ -16,11 +15,38 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
 
-    private lateinit var scenario: ActivityScenario<MainActivity>
+    private lateinit var mainActivityScenario: ActivityScenario<MainActivity>
 
     @Before
     fun setup() {
-        scenario = ActivityScenario.launch(MainActivity::class.java)
+        mainActivityScenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun toDetailsButton_Text_Checking() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(withText("TO DETAILS")))
+    }
+
+    @Test
+    fun searchEditText_Hint_Checking() {
+        val view = onView(withId(R.id.searchEditText))
+        val matcher = matches(withHint("Enter keyword e.g. android"))
+
+        view.check(matcher)
+    }
+
+    @Test
+    fun progressBar_isInvisible() {
+        onView(withId(R.id.progressBar))
+            .check(matches(withEffectiveVisibility(Visibility.GONE)))
+    }
+
+    @Test
+    fun navigateToDetails_Test() {
+        onView(withId(R.id.toDetailsActivityButton))
+            .perform(click())
+
+        onView(withId(R.id.incrementButton)).check(matches(isDisplayed()))
     }
 
     @Test
@@ -33,6 +59,6 @@ class MainActivityEspressoTest {
 
     @After
     fun close() {
-        scenario.close()
+        mainActivityScenario.close()
     }
 }
